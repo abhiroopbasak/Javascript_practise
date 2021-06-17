a1aad2cfcfac4ecb6b3c79bee3240030d10413b9
Abhiroop Basak <abhiroop1.bas@gmail.com>
2021-06-17 13:39:58 +0530

    main

---
+++ learn.js
@@ -0,0 +1,21 @@
+
+x=5;
+document.write(x);
+
+function al()
+{
+    alert('hi abhiroop');
+}
+
+function loc()
+{
+    a=document.location="https://www.google.com/";
+    alert(a);
+}
+
+function type()
+{
+    i=document.getElementById("in2");
+    j=document.getElementById("div1");
+    j.innerHTML=i.value;
+}
